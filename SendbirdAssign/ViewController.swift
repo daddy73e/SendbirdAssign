2071b05c926cdf7fef2819709ca36b480d4c723f
ygsong <ygsong@incruit.com>
2021-01-29 20:40:21 +0900

    first commit

---
+++ SendbirdAssign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SendbirdAssign
+//
+//  Created by ygsong on 2021/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SendbirdAssign/model/Book.swift
@@ -0,0 +1,17 @@
+//
+//  Book.swift
+//  SendbirdAssign
+//
+//  Created by ygsong on 2021/01/29.
+//
+
+import Foundation
+
+struct Book:Codable {
+    var title:String
+    var subtitle:String
+    var isbn13:String
+    var price:String
+    var image:String
+    var url:String
+}
