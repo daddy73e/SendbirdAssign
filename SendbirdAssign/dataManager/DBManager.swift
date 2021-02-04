657333a91aa1f0be7c0bc797b506b44336a285d8
ygsong <ygsong@incruit.com>
2021-02-04 15:18:13 +0900

    db test code

--- SendbirdAssign/dataManager/DBManager.swift
+++ SendbirdAssign/dataManager/DBManager.swift
@@ -17,7 +17,8 @@ class DBManager:NSObject {
     
     let createTableString = """
     CREATE TABLE Notes(
-    id INT PRIMARY KEY NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    isbn13 CHAR(255),
     content CHAR(255));
     """
     
@@ -49,9 +50,9 @@ class DBManager:NSObject {
         if sqlite3_prepare_v2(openDB(), createTableString, -1, &createTableStatement, nil) == SQLITE_OK {
             // 3
             if sqlite3_step(createTableStatement) == SQLITE_DONE {
-                print("Contact table created.")
+                print("Notes table created.")
             } else {
-                print("Contact table could not be created.")
+                print("Notes table could not be created.")
             }
         } else {
             print("CREATE TABLE statement could not be prepared.")
@@ -62,13 +63,14 @@ class DBManager:NSObject {
     public func insertNote(note:Note,
                            completion: @escaping (Bool) -> Void) {
         var insertStatement: OpaquePointer? = nil
-        let insertStatementString = "INSERT INTO Notes (id, content) VALUES (?, ?);"
+        let insertStatementString = "INSERT INTO Notes (id, isbn13, content) VALUES (?, ?, ?);"
         
         if sqlite3_prepare_v2(openDB(), insertStatementString, -1, &insertStatement, nil) == SQLITE_OK {
+            let isbn13:NSString = note.isbn13 as NSString
+            let content:NSString = note.content as NSString
             
-            let id:Int32 = Int32(note.isbn13) ?? 0
-            sqlite3_bind_int(insertStatement, 1, id)
-            sqlite3_bind_text(insertStatement, 2, note.content, -1, nil)
+            sqlite3_bind_text(insertStatement, 2, isbn13.utf8String, -1, nil)
+            sqlite3_bind_text(insertStatement, 3, content.utf8String, -1, nil)
             if sqlite3_step(insertStatement) == SQLITE_DONE {
                 completion(true)
             } else {
@@ -82,7 +84,9 @@ class DBManager:NSObject {
     
     public func updateNote(note:Note,
                            completion: @escaping (Bool) -> Void) {
-        let updateStatementString = "UPDATE Notes SET content = '\(note.content)' WHERE Id = \(note.isbn13);"
+        let updateStatementString = "UPDATE Notes SET content = '\(note.content)' WHERE isbn13 = '\(note.isbn13)';"
+        
+        print(updateStatementString)
         var updateStatement: OpaquePointer?
         if sqlite3_prepare_v2(openDB(), updateStatementString, -1, &updateStatement, nil) ==
             SQLITE_OK {
@@ -99,9 +103,9 @@ class DBManager:NSObject {
     
     public func deleteNote(isbn13:String,
                            completion: @escaping (Bool) -> Void) {
-        let deleteStatementStirng = "DELETE FROM Notes WHERE id = " + isbn13
+        let deleteStatementStirng = "DELETE FROM Notes WHERE isbn13 = '\(isbn13)'"
         var deleteStatement: OpaquePointer? = nil
-        
+        print(deleteStatementStirng)
         if sqlite3_prepare_v2(openDB(), deleteStatementStirng, -1, &deleteStatement, nil) == SQLITE_OK {
             if sqlite3_step(deleteStatement) == SQLITE_DONE {
                 completion(true)
@@ -114,7 +118,6 @@ class DBManager:NSObject {
         sqlite3_finalize(deleteStatement)
     }
     
-    
     public func readNotes(completion: @escaping ([Note]?) -> Void ) {
         let queryStatementString = "SELECT * FROM Notes;"
         var queryStatement: OpaquePointer? = nil
@@ -122,8 +125,16 @@ class DBManager:NSObject {
         if sqlite3_prepare_v2(openDB(), queryStatementString, -1, &queryStatement, nil) == SQLITE_OK {
             var notes = [Note]()
             while sqlite3_step(queryStatement) == SQLITE_ROW {
-                let isbn13 = "\(sqlite3_column_int(queryStatement, 0))"
-                let content = String(cString: sqlite3_column_text(queryStatement, 1))
+                let key = sqlite3_column_int(queryStatement, 0)
+                let isbn13 = String(cString: sqlite3_column_text(queryStatement, 1))
+                let content = String(cString: sqlite3_column_text(queryStatement, 2))
+                print("")
+                print("--------------")
+                print("key | \(key)")
+                print("isbn13 | \(isbn13)")
+                print("content | \(content)")
+                print("--------------")
+                print("")
                 let note = Note(isbn13: isbn13, content: content)
                 notes.append(note)
             }
@@ -135,4 +146,31 @@ class DBManager:NSObject {
         sqlite3_finalize(queryStatement)
     }
     
+    public func readNote(isbn13:String, completion: @escaping (Note?) -> Void ) {
+        let queryStatementString = "SELECT * FROM Notes WHERE isbn13 = '\(isbn13)';"
+        print(queryStatementString)
+        var queryStatement: OpaquePointer? = nil
+        
+        if sqlite3_prepare_v2(openDB(), queryStatementString, -1, &queryStatement, nil) == SQLITE_OK {
+            var note:Note?
+            while sqlite3_step(queryStatement) == SQLITE_ROW {
+                let key = sqlite3_column_int(queryStatement, 0)
+                let isbn13 = String(cString: sqlite3_column_text(queryStatement, 1))
+                let content = String(cString: sqlite3_column_text(queryStatement, 2))
+                print("")
+                print("--------------")
+                print("key | \(key)")
+                print("isbn13 | \(isbn13)")
+                print("content | \(content)")
+                print("--------------")
+                print("")
+                note = Note(isbn13: isbn13, content: content)
+            }
+            completion(note)
+        } else {
+            completion(nil)
+        }
+        
+        sqlite3_finalize(queryStatement)
+    }
 }
--- SendbirdAssignTests/SendbirdAssignTests.swift
+++ SendbirdAssignTests/SendbirdAssignTests.swift
@@ -12,7 +12,8 @@ class SendbirdAssignTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        
+        DBManager.instance.createTable()
+//        DBManager.instance.createTableTemp()
     }
     
     override func setUpWithError() throws {
@@ -56,6 +57,91 @@ class SendbirdAssignTests: XCTestCase {
         XCTAssertNotNil(resultObject)
     }
     
+    func testDB() {
+        testDBReadAll()
+        
+        print("--------------------- insert ---------------------")
+        let note0 = Note(isbn13: "9700000000001", content: "content 971")
+        let note1 = Note(isbn13: "9700000000002", content: "content 972")
+        let note2 = Note(isbn13: "9700000000003", content: "content 973")
+        DBManager.instance.insertNote(note: note0) { (isSuccess) in }
+        DBManager.instance.insertNote(note: note1) { (isSuccess) in }
+        DBManager.instance.insertNote(note: note2) { (isSuccess) in }
+        print("--------------------- insert Result ---------------------")
+        testDBReadAll()
+        
+        print("")
+        print("--------------------- upate ---------------------")
+        DBManager.instance.updateNote(note: Note(isbn13: "9700000000001",
+                                                 content: "update 971")) { (isSuccess) in }
+        print("--------------------- update Result ---------------------")
+        testDBReadAll()
+        
+        print("")
+        print("--------------------- delete ---------------------")
+        DBManager.instance.deleteNote(isbn13: "9700000000003") { (isSucces) in }
+        print("--------------------- delete Result ---------------------")
+        testDBReadAll()
+    }
     
+    func testDBInsert() {
+        testDBReadAll()
+        
+        let note0 = Note(isbn13: "9781617294136", content: "TEST 00000000000000000")
+        let note1 = Note(isbn13: "9781617291609", content: "TEST 11111111111111111")
+        
+        DBManager.instance.insertNote(note: note0) { (isSuccess) in
+            print("Insert DB Note1 result = \(isSuccess)")
+            print("")
+        }
+        
+        DBManager.instance.insertNote(note: note1) { (isSuccess) in
+            print("Insert DB Note2 result = \(isSuccess)")
+            print("")
+        }
+        
+        testDBReadAll()
+    }
+    
+    func testDBReadAll() {
+        DBManager.instance.readNotes { (notes) in
+            if notes?.count == 0 {
+                print("")
+                print("Empty DB")
+                print("")
+            }
+        }
+    }
+    
+    func testDBReadNote() {
+        testDBReadAll()
+        DBManager.instance.readNote(isbn13:"9781617294136") { (note) in
+            if let note = note {
+                print("Note = \(note)")
+                print("")
+            }
+        }
+    }
+    
+    func testDeleteNote() {
+        testDBReadAll()
+        DBManager.instance.deleteNote(isbn13: "9781617294136") { (isSuccess) in
+            print("Delete DB result = \(isSuccess)")
+            print("")
+        }
+        testDBReadAll()
+    }
 
+    func testUpdateNote() {
+        testDBReadAll()
+        let isbn13 = "9781617291609"
+        let content = "Update DB Update DBUpdate DB"
+        DBManager.instance.updateNote(note: Note(isbn13: isbn13,
+                                                 content: content)) { (isSuccess) in
+            print("Update DB id = \(isbn13) result = \(isSuccess)")
+            print("")
+        }
+        testDBReadAll()
+    }
+    
 }
